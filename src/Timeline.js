ba94ee9700f2713bbab3c57f3c92d8fa19bb4545
Juanca <juancasan17@gmail.com>
2019-11-14 20:39:03 -0600

    fix max minutes in slider

--- src/Timeline.js
+++ src/Timeline.js
@@ -8,13 +8,13 @@ const marks = {
   0: '6:00 AM',
   360: '12:00 PM',
   720: '6:00 PM',
-  1080: '9:00 PM',
+  900: '9:00 PM',
 };
 
 class Timeline extends React.Component {
   componentDidUpdate() {
     const { play, speed, current } = this.props
-    if (play && current < 1080) {
+    if (play && current < 900) {
       setTimeout(() => {
         this.props.updateCurrentTime(current + 1)
       }, speed);
@@ -31,7 +31,7 @@ class Timeline extends React.Component {
   render() {
     const { current } = this.props
     return (
-      <Slider marks={marks} value={current} max={1080} tooltipVisible={false} onChange={this.handleOnChange} />
+      <Slider marks={marks} value={current} max={900} tooltipVisible={false} onChange={this.handleOnChange} />
     )
   }
 }
